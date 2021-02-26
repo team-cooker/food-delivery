4e944eaf824f8054a341a4e5b8e5703fd591d5a0
DESKTOP-5SH92JR\강길환 <rlfghksop@naver.com>
2021-02-26 19:29:08 +0900

    feat: Add Store entity fields

--- food-delivery-core/src/main/java/com/cooker/fooddelivery/core/store/entity/Store.kt
+++ food-delivery-core/src/main/java/com/cooker/fooddelivery/core/store/entity/Store.kt
@@ -1,9 +1,30 @@
 package com.cooker.fooddelivery.core.store.entity
 
 import com.cooker.fooddelivery.core.common.entity.EntityAuditing
+import com.cooker.fooddelivery.core.store.type.CustomerReceiveType
+import com.cooker.fooddelivery.core.store.type.FoodCategoryType
+import com.cooker.fooddelivery.core.store.type.MeetPayType
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 
 @Entity
 class Store(
-        var name: String
+    var name: String,
+
+    @Enumerated(EnumType.STRING)
+    val category: FoodCategoryType,
+    val bizRegNo: String,
+    var tel: String,
+    var address: String,
+    var description: String,
+    var minOrderAmount: Int,
+    var deliveryTip: Int,
+    var alcoholicBeverageYn: Int,
+
+    @Enumerated(EnumType.STRING)
+    var meetPayType: MeetPayType,
+
+    @Enumerated(EnumType.STRING)
+    var customerReceiveType: CustomerReceiveType
 ) : EntityAuditing()
---
+++ food-delivery-core/src/main/java/com/cooker/fooddelivery/core/store/type/FoodCategoryType.kt
@@ -0,0 +1,12 @@
+package com.cooker.fooddelivery.core.store.type
+
+enum class FoodCategoryType {
+    FAST_FOOD,
+    CAFE_DESSERT,
+    LUNCH_BOX,
+    STEAMED_SOUP,
+    PIZZA,
+    CHICKEN,
+    CHINESE_FOOD,
+    WESTERN_FOOD
+}
